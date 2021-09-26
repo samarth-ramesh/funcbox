143ffec72a175346c92b75b90a4a6e98885c338b
Samarth Ramesh <samarthr1@outlook.com>
2021-09-26 16:56:47 +0530

    got p2 done

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./Header/Header";
 import P1 from "./p1/P1";
+import P2 from "./p2/P2";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <P1/>
+            <P2/>
         </div>
     );
 }
---
+++ src/p2/P2.tsx
@@ -0,0 +1,82 @@
+import react, {useState} from "react"
+import "./p2.css"
+
+function Item(props: {
+    colorClassName: string, title: string, children: react.ReactElement,
+    buttonColorClassName: string, translateOpenClassName: string,
+    translateCloseClassName: string
+}) {
+
+    const [open, setOpen] = useState(false)
+    const ocf = () => {
+        setOpen(!open)
+    } // toggling open
+
+    return <div className={"p2_plans_all_item_holder"} onClick={ocf}>
+        <div className={props.colorClassName + " p2_plans_all_items"}>
+            <div className={"p2_plans_item_title"}>{props.title}</div>
+            <div className={"p2_plans_item_text"}>
+                {props.children}
+            </div>
+        </div>
+        <ul className={"p2_plans_all_pricing p2_plans_all_transition " + (open ? props.translateOpenClassName : props.translateCloseClassName)}>
+            <li>Monthly a/month</li>
+            <li>Quarterly a/month</li>
+            <li>Half-yearly a/month</li>
+            <li>Yearly a/month</li>
+            <button className={props.buttonColorClassName + " p2_plans_all_button"}>Add to cart</button>
+        </ul>
+    </div>
+}
+
+function P2() {
+    return <>
+        <div className={"p2_title_text"}>Plans</div>
+        <div className={"p2_plans_holder"}>
+            <Item colorClassName={"p2_plans_basic_color"} title={"Basic"} buttonColorClassName={"p2_plans_basic_button"}
+                  translateCloseClassName={"p2_plans_basic_closed"} translateOpenClassName={"p2_plans_basic_open"}>
+                <ul>
+                    <li>FuncBox Binder</li>
+                    <li>3 mathematical apps</li>
+                    <li>3 scientific apps</li>
+                    <li>3 game apps</li>
+                    <li>Stickers</li>
+                    <li>Kickstart guide</li>
+                    <li>Interactive website</li>
+                </ul>
+            </Item>
+            <Item colorClassName={"p2_plans_standard_color"} title={"Standard"}
+                  buttonColorClassName={"p2_plans_standard_button"} translateOpenClassName={"p2_plans_standard_open"}
+                  translateCloseClassName={"p2_plans_standard_closed"}>
+                <ul>
+                    <li>FuncBox Binder</li>
+                    <li>5 mathematical apps</li>
+                    <li>5 scientific apps</li>
+                    <li>5 game apps</li>
+                    <li>Stickers</li>
+                    <li>Mini fun deck</li>
+                    <li>Kickstart guide</li>
+                    <li>Interactive website</li>
+                </ul>
+            </Item>
+            <Item colorClassName={"p2_plans_premium_color"} title={"Premium"}
+                  buttonColorClassName={"p2_plans_premium_button"} translateOpenClassName={"p2_plans_premium_open"}
+                  translateCloseClassName={"p2_plans_premium_closed"}>
+                <ul>
+                    <li>FuncBox Binder</li>
+                    <li>8 mathematical apps</li>
+                    <li>8 scientific apps</li>
+                    <li>8 game apps</li>
+                    <li>2 Surprise goodies</li>
+                    <li>Mystery toy</li>
+                    <li>Stickers</li>
+                    <li>Mini fun deck</li>
+                    <li>Kickstart guide</li>
+                    <li>Interactive website</li>
+                </ul>
+            </Item>
+        </div>
+    </>
+}
+
+export default P2
