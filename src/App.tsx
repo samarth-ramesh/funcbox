2165263a6f38694d7365662a3945bd00e4050bce
Samarth Ramesh <samarthr1@outlook.com>
2021-09-26 21:24:17 +0530

    added header and footer to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import LandingPage from "./LandingPage/LandingPage";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch,} from "react-router-dom";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
 function App() {
     return (
         <Router>
+            <Header/>
             <Switch>
                 <Route exact path={"/"}><LandingPage/></Route>
             </Switch>
+            <Footer/>
         </Router>
     );
 }
--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -1,21 +1,17 @@
-import Header from "../Header/Header";
 import P1 from "./p1/P1";
 import P2 from "./p2/P2";
 import P3 from "./p3/P3";
 import P4 from "./p4/P4";
-import Footer from "../Footer/Footer";
 import React from "react";
 
 function LandingPage() {
     return (
-        <div>
-            <Header/>
+        <>
             <P1/>
             <P2/>
             <P3/>
             <P4/>
-            <Footer/>
-        </div>
+        </>
     )
 }
 
