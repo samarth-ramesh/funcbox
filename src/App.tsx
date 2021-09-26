028712359901bfbd1946f2ff1b23bc7fe6014e88
Samarth Ramesh <samarthr1@outlook.com>
2021-09-26 18:56:56 +0530

    added for schools

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./Header/Header";
 import P1 from "./p1/P1";
 import P2 from "./p2/P2";
 import P3 from "./p3/P3";
+import P4 from "./p4/P4";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <P1/>
             <P2/>
             <P3/>
+            <P4/>
         </div>
     );
 }
---
+++ src/p4/P4.tsx
@@ -0,0 +1,25 @@
+import "./p4.css"
+
+export default function (){
+    return (
+        <>
+            <div className={"p4_title"}>For Schools</div>
+            <div className={"p4_holder"}>
+                <img src={"/images/filler-raster.png"} alt={"filler"}/>
+                <div className={"p4_text_holder"}>
+                    <p className={"p4_text"}>
+                        Yay! FuncBox brings special discount for
+                        all the School Tie-ups. FuncBox can be
+                        easily incorporated in all the school grades.
+                        To make your students outperform and
+                        help your institute gain an edge over the
+                        others contact our team NOW!
+                    </p>
+                    <p>
+                        <a>Know more</a>
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
